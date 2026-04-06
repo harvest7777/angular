e18e42194586ecc0339e1d62ca3fe2d844a41c3e
harvest7777 <333harvest.moon333@gmail.com>
2026-04-05 18:21:25 -0700

    feat: configure admin route for UserProfile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserProfile } from './user-profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    component: UserProfile,
+  },
+];
